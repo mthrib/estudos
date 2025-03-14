a3bfdaf84932cd0d872bc7a2b8129a6f023cd967
mthrib <matheuseyorrana@gmail.com>
2025-03-14 18:40:37 -0300

    Update boletim.py

--- boletim.py
+++ boletim.py
@@ -1,26 +1,26 @@
 def obter_nota(mensagem):
-    while true:
+    while True:  # "True" com "T" maiúsculo
         try:
-            nota - float(input(mensagem))
+            nota = float(input(mensagem))  # "=" ao invés de "-"
             if 0 <= nota <= 10:
                 return nota
             else:
-                print("Erro: A nota deve estar entre 0 e 10> Tente Novamente!")
+                print("Erro: A nota deve estar entre 0 e 10. Tente novamente!")
         except ValueError:
-            print("ERRO: Insira um valor válido")
+            print("ERRO: Insira um valor válido.")
 
 # Dados
 nome = input("Nome do aluno: ")
-notaprova = float(input("Qual sua nota da prova? "))
-notateste = float(input("Qual sua nota do teste?"))
-notaav = float(input("Nota de Atividade Avaliativa"))
+notaprova = obter_nota("Qual sua nota da prova? ")  # Usa a função para garantir limite
+notateste = obter_nota("Qual sua nota do teste? ")
+notaav = obter_nota("Nota de Atividade Avaliativa: ")
 
-#cálculo da nota
-notaf = 2 * notaprova + notateste +  4 * notaav 
+# Cálculo da nota
+notaf = 2 * notaprova + notateste + 4 * notaav 
 notafinal = notaf / 7
 
-#exibição do boletim
-print("Nome do aluno","|", "NP","|", "NT","|", "NAV","|", "Nota Final")
-print("================================================")
+# Exibição do boletim
+print("\nNome do aluno | NP | NT | NAV | Nota Final")
+print("=========================================")
 print(f"{nome} | {notaprova:.1f} | {notateste:.1f} | {notaav:.1f} | {notafinal:.2f}")
-print("================================================")
+print("=========================================")
