7345407837d4f66650a31e7977c507593e7547e6
mthrib <matheuseyorrana@gmail.com>
2025-03-14 18:28:39 -0300

    Update boletim.py

--- boletim.py
+++ boletim.py
@@ -1,15 +1,26 @@
+def obter_nota(mensagem):
+    while true:
+        try:
+            nota - float(input(mensagem))
+            if 0 <= nota <= 10:
+                return nota
+            else:
+                print("Erro: A nota deve estar entre 0 e 10> Tente Novamente!")
+        except ValueError:
+            print("ERRO: Insira um valor válido")
+
 # Dados
-nome = input("Nome do aluno:")
+nome = input("Nome do aluno: ")
 notaprova = float(input("Qual sua nota da prova? "))
-notatste = float(input("Qual sua nota do teste?"))
+notateste = float(input("Qual sua nota do teste?"))
 notaav = float(input("Nota de Atividade Avaliativa"))
 
-#cálculo
-notaf = 2 * notaprova + notatste +  4 * notaav 
+#cálculo da nota
+notaf = 2 * notaprova + notateste +  4 * notaav 
 notafinal = notaf / 7
 
-#tabela
-print("Nome do aluno","/", "NP","/", "NT","/", "NAV","/", "Nota Final")
+#exibição do boletim
+print("Nome do aluno","|", "NP","|", "NT","|", "NAV","|", "Nota Final")
 print("================================================")
-print( nome,  notaprova,"/",     notatste,"/",   notaav, "/", notafinal)
+print(f"{nome} | {notaprova:.1f} | {notateste:.1f} | {notaav:.1f} | {notafinal:.2f}")
 print("================================================")
